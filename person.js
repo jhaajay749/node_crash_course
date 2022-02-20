5e3961a1f4c90c7d16cfab9d4517155256bd95b5
jhaajay749 <jhaajay749@gmail.com>
2022-02-20 19:51:19 +0530

    node crash course

---
+++ person.js
@@ -0,0 +1,21 @@
+/////////////////////////////////////
+const person = {
+  name: "Ajay",
+  age: 30,
+};
+
+module.exports = person;
+
+/////////////////////////////////////////
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greeting() {
+    console.log(`My name is ${this.name} and I am ${this.age}`);
+  }
+}
+
+module.exports = Person;
